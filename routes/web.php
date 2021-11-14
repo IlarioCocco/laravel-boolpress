591abaef9fdf5a8dd528ac65475781ed7d11ff0a
IlarioCOCCO <ilario.cocco@gmail.com>
2021-11-14 18:46:03 +0100

    commit learning lesson

--- routes/web.php
+++ routes/web.php
@@ -13,18 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Rotte Pubbliche
+//Rotte Pubbliche che non necessitano di Autenticazione
 Route::get('/', function () {
     return view('welcome');
 });
 
 
-//Rotte di Autenticazione che ci fornisce Laravel
+//Rotte di Autenticazione che ci fornisce Laravel per far Funzionare tutto(login,autentcazione,cambio password logout, ecc..)
 Auth::routes();
 
 
-//Rotte Nostra Area Admin
-Route::middleware('auth')
+//Zona Rotte Nostra Area Admin
+Route::middleware('auth') //funzione di controllo login/logout, verifica se l'utente è autentificato
 ->namespace('Admin') //il namespace è una Cartella che conterrà tutti i nostri Controller, ovvero (Cartella Admin)
 ->name('admin.') //il name è il nome delle nostre Rotte (es: admin.home, vedi sotto)
 ->prefix('admin') // il prefisso è /admin/ stesso, divide le url del(Front office:localhost:8000) dal(Back office /admin/home)

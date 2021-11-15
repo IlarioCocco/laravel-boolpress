184dffbda705e2587496b3d13bd4c5e86f05b047
IlarioCOCCO <ilario.cocco@gmail.com>
2021-11-15 18:11:55 +0100

    debug color

--- resources/views/guest/posts/show.blade.php
+++ resources/views/guest/posts/show.blade.php
@@ -1,31 +1,13 @@
 @extends('layouts.guest')
 @section('pageContent')
 
-
       <div class="row">
         <div class="col-md-8 blog-main">
 
-
-        @foreach ($posts as $post)
-            <div class="blog-post">
-                <h2 class="blog-post-title">{{$post["title"]}}</h2>
-                <p class="blog-post-meta">{{$post->created_at->diffForHumans()}}<a href="#">Jacob</a></p>
-                 <p>
-                    {{$post["content"]}}
-                </p>
-            </div><!-- /.blog-post -->
-        @endforeach
-
-
-          <nav class="blog-pagination">
-            <a class="btn btn-outline-primary" href="#">Older</a>
-            <a class="btn btn-outline-secondary disabled" href="#">Newer</a>
-          </nav>
-
         </div><!-- /.blog-main -->
 
-        <aside class="col-md-4 blog-sidebar">
-          <div class="p-3 mb-3 bg-light rounded">
+        <aside class="col-md-4 blog-sidebar ">
+          <div class="p-3 mb-3 bg-light rounded ">
             <h4 class="font-italic">About</h4>
             <p class="mb-0">Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
           </div>
@@ -50,9 +32,5 @@
         </aside><!-- /.blog-sidebar -->
 
       </div><!-- /.row -->
-
     
 @endsection
-
-
-

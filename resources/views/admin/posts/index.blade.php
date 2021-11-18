fc50b971d3e673312e89fffd25972eb5aade558e
IlarioCOCCO <ilario.cocco@gmail.com>
2021-11-18 20:07:25 +0100

    add modal type

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -51,6 +51,17 @@
                                         <button type="submit" class="btn btn-danger">Cancel</button>
                                     </form>
                                 </td>
+
+
+                                <td>
+                                    <form action="{{route("admin.posts.destroy", $post["id"])}}" method="POST">
+                                        @csrf
+                                        @method("DELETE")
+                                        <!-- Button trigger modal -->
+                                        <button type="button" class="btn btn-dark" data-toggle="modal" data-target="#exampleModal">Elimina</button>
+                                    </form>
+                                </td>
+
                                 
                             </tr>
                             @endforeach
@@ -60,5 +71,31 @@
             </div>
         </div>
     </div>
+</div>
+
+{{-- <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Launch demo modal
+</button> --}}
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">vuoi confermare la Cancellazione ?</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
 </div>
 @endsection
